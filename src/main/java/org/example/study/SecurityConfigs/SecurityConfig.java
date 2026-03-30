1175cfdac03ec81b19b524a0d9e9b6a178505acb
Razvan503 <ciobanurazvan503@gmail.com>
2026-03-30 18:37:48 +0300

    Reorganize imports in SecurityConfig.java

--- src/main/java/org/example/study/SecurityConfigs/SecurityConfig.java
+++ src/main/java/org/example/study/SecurityConfigs/SecurityConfig.java
@@ -11,10 +11,10 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter; // Use this import
-import org.springframework.web.cors.CorsConfiguration; // Import this
-import org.springframework.web.cors.CorsConfigurationSource; // Import this
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource; // Import this
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter; 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource; 
 
 import java.util.List;
 
